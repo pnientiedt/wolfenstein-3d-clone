3e245ddce84ca49ef3e7e0ac60a2d6f868034c99
Phillip Nientiedt <phillipnientiedt@gmail.com>
2014-03-29 18:29:22 +0000

    Wolfenstein #7: Player movement

--- src/com/base/engine/Game.java
+++ src/com/base/engine/Game.java
@@ -5,23 +5,28 @@ package com.base.engine;
 public class Game {
 	
 	private Level level;
+	private Player player;
 
 	public Game() {
-		
-		Transform.setCamera(new Camera());
-		Transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.01f, 1000f);
 		level = new Level("level1.png", "WolfCollection.png");
+		player = new Player(new Vector3f(0,0.4375f,0));
+		
+		Transform.setCamera(player.getCamera());
+		Transform.setProjection(70, Window.getWidth(), Window.getHeight(), 0.01f, 1000f);
 	}
 
 	public void input() {
-		Transform.getCamera().input();
+		level.input();
+		player.input();
 	}
 
 	public void update() {
-		
+		level.update();
+		player.update();
 	}
 
 	public void render() {
 		level.render();
+		player.render();
 	}
 }
