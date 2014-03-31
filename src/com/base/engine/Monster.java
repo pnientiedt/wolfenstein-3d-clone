9d41446cb558c9d35041b3ea4611824ad6a08b4d
Phillip Nientiedt <phillipnientiedt@gmail.com>
2014-03-31 18:40:14 +0000

    Hacked Sprites

--- src/com/base/engine/Monster.java
+++ src/com/base/engine/Monster.java
@@ -10,19 +10,29 @@ public class Monster {
 	public final static float OFFSET_X = 0.01f;
 	public final static float OFFSET_Y = 0.01f;
 	
+	public final static float OFFSET_FROM_GROUND = 0.7f;
+	
 	public static final float TEX_MIN_X = -OFFSET_X;
 	public static final float TEX_MAX_Y = -1 -OFFSET_X;
 	public static final float TEX_MIN_Y = -OFFSET_Y;
 	public static final float TEX_MAX_X = 1 - OFFSET_Y;
 	
+	public static final int STATE_IDLE = 0;
+	public static final int STATE_CHASE = 1;
+	public static final int STATE_ATTACK = 2;
+	public static final int STATE_DYING = 3;
+	public static final int STATE_DEAD = 4;
+	
 	private static Mesh mesh;
 	private Material material;
 	private Transform transform;
+	private int state;
 	
 	public Monster(Transform transform) {
 		this.transform = transform;
+		this.state = STATE_IDLE;
 		this.transform.setRotation(this.transform.getRotation().add(new Vector3f(0,0,180)));
-		this.transform.setTranslation(this.transform.getTranslation().add(new Vector3f(0,0.7f,0)));
+//		this.transform.setTranslation(this.transform.getTranslation().add(new Vector3f(0,0.7f,0)));
 		material = new Material(new Texture("SSWVA1.png"));
 		if(mesh == null) {
 			
@@ -38,8 +48,52 @@ public class Monster {
 		}
 	}
 	
-	public void update() {
+	private void idleUpdate() {
 		
+	}
+	
+	private void chaseUpdate() {
+		
+	}
+	
+	private void attackUpdate() {
+		
+	}
+	
+	private void dyingUpdate() {
+		
+	}
+	
+	private void deadUpdate() {
+		
+	}
+	
+	private void alignWithGround() {
+		transform.getTranslation().setY(OFFSET_FROM_GROUND);
+	}
+	
+	public void faceCamera() {
+		Vector3f directionToCamera = transform.getTranslation().sub(Transform.getCamera().getPos());
+		float angleToFaceTheCamera = (float) Math.toDegrees(Math.atan(directionToCamera.getZ() / directionToCamera.getX()));
+		
+		if (directionToCamera.getX() > 0) {
+			angleToFaceTheCamera += 180;
+		}
+		
+		transform.getRotation().setY(-(angleToFaceTheCamera + 90));
+	}
+	
+	public void update() {
+		alignWithGround();
+		faceCamera();		
+		
+		switch(state) {
+			case STATE_IDLE: idleUpdate(); break;
+			case STATE_CHASE: chaseUpdate(); break;
+			case STATE_ATTACK: attackUpdate(); break;
+			case STATE_DYING: dyingUpdate(); break;
+			case STATE_DEAD: deadUpdate(); break;
+		}
 	}
 	
 	public void render() {
